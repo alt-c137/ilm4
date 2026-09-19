ee0456c5e0c2678c34bf1dd2bc0d07d9257e6456
Shoha <alter.shoha@gmail.com>
2026-09-20 01:11:20 +0500

    ruff: сортировка импортов

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
-from .models import Rate
 from .blocks import get_blocks
-from .models import ModuleConfig
+from .models import ModuleConfig, Rate
 
 
 def home(request):
